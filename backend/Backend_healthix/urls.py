5eec543509806b6b62f648d8c5bf3b0de6147dbd
vincent ouma <vinceoumah@gmail>
2020-02-27 00:08:39 +0300

    created online lipa url

--- backend/Backend_healthix/urls.py
+++ backend/Backend_healthix/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('access/token', views.getAccessToken, name='get_mpesa_access_token'),
+    url('online/lipa', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
+
 
 ]
