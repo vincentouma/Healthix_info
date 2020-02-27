57fea93009f4849e42055a3e1a5fc41ec238fce7
vincent ouma <vinceoumah@gmail>
2020-02-27 18:08:20 +0300

    created rigister,confirmation and validation urls

--- backend/Backend_healthix/urls.py
+++ backend/Backend_healthix/urls.py
@@ -6,5 +6,9 @@ urlpatterns=[
     url('access/token', views.getAccessToken, name='get_mpesa_access_token'),
     url('online/lipa', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
 
-
+# register, confirmation, validation and callback urls
+    url('c2b/register', views.register_urls, name="register_mpesa_validation"),
+    url('c2b/confirmation', views.confirmation, name="confirmation"),
+    url('c2b/validation', views.validation, name="validation"),
+    url('c2b/callback', views.call_back, name="call_backurl"),
 ]
