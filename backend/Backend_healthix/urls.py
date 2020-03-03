159f85d1c90a72633d98e77a501d80defabc935c
vincent ouma <vinceoumah@gmail>
2020-03-03 20:14:30 +0300

    url for combined bill

--- backend/Backend_healthix/urls.py
+++ backend/Backend_healthix/urls.py
@@ -11,4 +11,16 @@ urlpatterns=[
     url('c2b/confirmation', views.confirmation, name="confirmation"),
     url('c2b/validation', views.validation, name="validation"),
     url('c2b/callback', views.call_back, name="call_backurl"),
+
+# payment, combinedbills
+
+    url('^combinedReport/', views.combinedReport, name='combinedReport'),
+    url('^payment', views.payment, name='payment'),
+
+# API 
+
+    url('^api/bills/', views.BillsList.as_view()),
+    url('^all_customer_bills/', views.all_customer_bills, name='all_customer_bills'),
+
+
 ]
